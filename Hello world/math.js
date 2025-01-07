dba1388366185099afa12b175fa9e24827d6cee9
Vikram-singh0 <vsvikramsingh305@gmail.com>
2025-01-08 00:00:05 +0530

    creating and using modules

---
+++ Hello world/math.js
@@ -0,0 +1,19 @@
+// function add(a,b){
+// return a+b
+// }
+// function sub(a,b){
+//  return  a-b
+// }
+
+// module.exports={
+//   add,
+//   sub
+// }
+
+exports.add=(a,b)=>{
+  return a+b
+}
+
+exports.sub=(a,b)=>{
+  return a-b
+}

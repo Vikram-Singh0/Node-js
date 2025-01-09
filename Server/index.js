b709abe29176c31509082f22ec3aef2c024101ef
Vikram-singh0 <vsvikramsingh305@gmail.com>
2025-01-09 22:17:30 +0530

    Handling URLs

--- Server/index.js
+++ Server/index.js
@@ -1,17 +1,21 @@
 const http = require("http");
 const fs = require("fs");
+const url=require('url')
 
 const myserver = http.createServer((req, res) => {
-  const log = `${Date.now()} ${req.url}:New req received`;
+  const log = `${Date.now()}: ${req.url}New req received\n`;
+  const myUrl=url.parse(req.url,true)
+  console.log(myUrl)
   fs.appendFile("./log.txt", log, (err, data) => {
     if (err) {
       console.error(err);
       res.writeHead(500, { 'Content-Type': 'text/plain' });
       res.end('Internal Server Error');
     } else {
-      switch (req.url) {
+      switch (myUrl.pathname) {
         case "/":
-          res.end("Welcome home Vikram ");
+          const user=myUrl.query.name
+          res.end(`Hello ${user}ji`);
           break;
 
         case "/about":
